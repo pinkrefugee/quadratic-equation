3a52b27b0c3083c0863c5a89110ce599b79526ad
pinkrefugee <pinkrefugee@gmail.com>
2018-02-24 00:14:12 +0300

    equation solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+
+equation = equation.replace(/\s/g, '');
+
+var numbers = equation.match(/-?\d+/g);
+
+var a, b, c;
+var arr = [];
+
+a = numbers[0];
+b = numbers[2];
+c = numbers[3];
+
+var d = Math.pow(b,2) - 4*a*c;
+if ( d > 0)
+{
+  var root1, root2, a1;
+  root1 = Math.round((-b - Math.sqrt(d))/(2*a));
+  root2 = Math.round((-b + Math.sqrt(d))/(2*a));
+}
+
+arr.push(root1);
+arr.push(root2);
+function compareNumbers(a, b) {
+  return a - b;
+}
+arr.sort(compareNumbers);
+return arr;
 }
